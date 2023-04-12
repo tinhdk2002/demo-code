d0c505ed052ce92c0c05c3424d6900eb4da8e3a4
tinhdk2002 <tinhdk2002@gmail.com>
2023-04-12 17:41:44 +0700

    add DataProvider.cs file

---
+++ DataProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhHocPhi
+{
+    internal class DataProvider
+    {
+        private readonly string connectionSTR = "Data Source=.\\sqlexpress;Initial Catalog=TinhHocPhi;Integrated Security=True";
+
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable data = new();
+
+            using (SqlConnection connection = new(connectionSTR))
+            {
+                connection.Open();
+
+                SqlCommand command = new(query, connection);
+
+                SqlDataAdapter adapter = new(command);
+
+                adapter.Fill(data);
+
+                connection.Close();
+            }
+
+            return data;
+        }
+    }
+}
